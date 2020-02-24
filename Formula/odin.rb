7fdebc33a2282e1fdf2237af1697fe94e6fac186
SeekingMeaning <meaningseeking@protonmail.com>
2020-02-23 17:36:58 -0800

    odin 0.12.0

--- Formula/odin.rb
+++ Formula/odin.rb
@@ -1,6 +1,9 @@
 class Odin < Formula
   desc "Simple, high-performance programming language built for modern systems"
   homepage "https://odin-lang.org/"
+  url "https://github.com/odin-lang/Odin.git",
+      :tag      => "v0.12.0",
+      :revision => "14c4fed94ca0a401892409c6a4e8a018e65b0e4c"
   head "https://github.com/odin-lang/Odin.git"
 
   depends_on "llvm"
