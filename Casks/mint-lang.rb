ebf66f0b976d6b28014e30cc50548e369b00d5f9
SeekingMeaning <meaningseeking@protonmail.com>
2020-02-24 07:55:47 -0800

    mint-lang 0.7.1 (new cask)

---
+++ Casks/mint-lang.rb
@@ -0,0 +1,12 @@
+cask 'mint-lang' do
+  version '0.7.1'
+  sha256 '5913f26524ffffeebdaa817c4e068e5dde3de6779e1631043aa1b18707bdf6a4'
+
+  url "https://github.com/mint-lang/mint/releases/download/#{version}/mint-#{version}-osx"
+  name 'Mint'
+  homepage 'https://www.mint-lang.com/'
+
+  conflicts_with formula: 'mint-lang'
+
+  binary "mint-#{version}-osx", target: 'mint'
+end
