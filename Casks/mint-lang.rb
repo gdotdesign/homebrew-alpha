5c2863b7cae4697716d97d14ccf1a917b44a8f13
SeekingMeaning <meaningseeking@protonmail.com>
2020-02-24 08:07:24 -0800

    mint-lang (cask): add appcast

--- Casks/mint-lang.rb
+++ Casks/mint-lang.rb
@@ -3,6 +3,7 @@ cask 'mint-lang' do
   sha256 '5913f26524ffffeebdaa817c4e068e5dde3de6779e1631043aa1b18707bdf6a4'
 
   url "https://github.com/mint-lang/mint/releases/download/#{version}/mint-#{version}-osx"
+  appcast 'https://github.com/mint-lang/mint/releases.atom'
   name 'Mint'
   homepage 'https://www.mint-lang.com/'
 
