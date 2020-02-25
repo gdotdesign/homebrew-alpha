1a34fc171a5045137652141f559213796ff3bf14
SeekingMeaning <meaningseeking@protonmail.com>
2020-02-25 11:26:53 -0800

    zig: add linux

--- Casks/zig.rb
+++ Casks/zig.rb
@@ -1,13 +1,28 @@
 cask 'zig' do
   version '0.5.0'
-  sha256 '28702cc05745c7c0bd450487d5f4091bf0a1ad279b35eb9a640ce3e3a15b300d'
 
-  url "https://ziglang.org/download/#{version}/zig-macos-x86_64-#{version}.tar.xz"
+  if OS.mac?
+    sha256 '28702cc05745c7c0bd450487d5f4091bf0a1ad279b35eb9a640ce3e3a15b300d'
+    url "https://ziglang.org/download/#{version}/zig-macos-x86_64-#{version}.tar.xz"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      sha256 '43e8f8a8b8556edd373ddf9c1ef3ca6cf852d4d09fe07d5736d12fefedd2b4f7'
+      url "https://ziglang.org/download/#{version}/zig-linux-x86_64-#{version}.tar.xz"
+    end
+  end
+
   name 'Zig'
   homepage 'https://ziglang.org/'
 
-  conflicts_with formula: 'zig', # conflicts_with formula: is not yet functional
+  conflicts_with formula: 'zig',
                  cask:    'zig@master'
+  depends_on formula: 'llvm'
 
-  binary "zig-macos-x86_64-#{version}/zig"
+  if OS.mac?
+    binary "zig-macos-x86_64-#{version}/zig"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      binary "zig-linux-x86_64-#{version}/zig"
+    end
+  end
 end
--- Casks/zig@master.rb
+++ Casks/zig@master.rb
@@ -1,8 +1,25 @@
 require 'open-uri'
 
+cpu = nil
+os = nil
+if OS.mac?
+  cpu = 'x86_64'
+  os = 'macos'
+elsif OS.linux?
+  if Hardware::CPU.intel?
+    cpu = 'x86_64'
+    os = 'linux'
+  elsif Hardware::CPU.arm?
+    if Hardware::CPU.is_64_bit?
+      cpu = 'aarch64'
+      os = 'linux'
+    end
+  end
+end
+
 releases = JSON.load(open('https://ziglang.org/download/index.json'))
-sha256 = releases['master']['x86_64-macos']['shasum']
-url = releases['master']['x86_64-macos']['tarball']
+sha256 = releases['master']["#{cpu}-#{os}"]['shasum']
+url = releases['master']["#{cpu}-#{os}"]['tarball']
 # e.g. zig-macos-x86_64-0.5.0+d1243bf27
 m = %r{-((?:\d+\.)*\d+\.\d+\+[a-fA-F0-9]{9})}.match(url)
 version = m.captures.first
@@ -15,8 +32,9 @@ cask 'zig@master' do
   name 'Zig'
   homepage 'https://ziglang.org/'
 
-  conflicts_with formula: 'zig', # conflicts_with formula: is not yet functional
+  conflicts_with formula: 'zig',
                  cask:    'zig'
+  depends_on formula: 'llvm'
 
-  binary "zig-macos-x86_64-#{version}/zig"
+  binary "zig-#{os}-#{cpu}-#{version}/zig"
 end
