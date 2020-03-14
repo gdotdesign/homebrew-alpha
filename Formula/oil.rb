a3680b5bf5d82158754db5498d75a0b7c5161256
SeekingMeaning <meaningseeking@protonmail.com>
2020-03-14 09:00:06 -0700

    oil 0.8.pre2

--- Formula/oil.rb
+++ Formula/oil.rb
@@ -1,8 +1,8 @@
 class Oil < Formula
   desc "New Unix shell"
   homepage "https://www.oilshell.org/"
-  url "https://www.oilshell.org/download/oil-0.8.pre1.tar.xz"
-  sha256 "d42de21c3076aa7a93ebbe8e2026ea6f9fd4d5097130272365d1e42103f70fea"
+  url "https://www.oilshell.org/download/oil-0.8.pre2.tar.xz"
+  sha256 "671dccc73804c37b709d1cc203202957c631c8173052280083dad389514ed79f"
 
   def install
     system "./configure", "--prefix=#{prefix}"
