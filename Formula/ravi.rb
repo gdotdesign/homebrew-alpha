b50bc592deadc8e855cb329586fb95c413ac8b14
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-17 12:22:03 -0700

    ravi: update 1.0-beta3 bottle.

--- Formula/ravi.rb
+++ Formula/ravi.rb
@@ -5,6 +5,12 @@ class Ravi < Formula
   sha256 "4eb936dff0ea4e03d7cb35a305cd1b4224e575e7df31378b7cc141d4b7c9c91a"
   head "https://github.com/dibyendumajumdar/ravi.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/homebrew-community/bottles-alpha"
+    cellar :any
+    sha256 "06f1dabe5a021c604a0892c3b89c7e57998c8f9f4e91c7f22b03801254622a85" => :catalina
+  end
+
   depends_on "cmake" => :build
   depends_on "llvm" => :recommended
 
